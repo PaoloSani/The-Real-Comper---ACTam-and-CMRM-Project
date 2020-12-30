9f510cf1f13a362e7d1a46dab8d161d0cc492bfd
Andrés González Paul Rivera <andresgpaul@MacBook-Pro-de-Andres.local>
2020-12-30 14:54:35 +0100

    improved the main structure

--- buttons.js
+++ buttons.js
@@ -9,11 +9,6 @@ real_btn.addEventListener("change", function() {
     const mid_val = real_btn.value;
 });
 
-// function save() { 
-//     val = document.myform.text_area.value; 
-//     mydoc = document.open(); 
-//     mydoc.write(val); 
-//     mydoc.execCommand("saveAs",true,".mid"); //you can change the .txt to your extention
-//     history.go(-1);
-// }
+var meter = document.getElementById("meters").value;
 
+var tempo = document.getElementById("bpm").value;
