0276d553b9e7c7ad8a10c55428f4bb0aed96c63b
Paolo <paolo.sani98@gmail.com>
2020-12-30 17:22:45 +0100

    First lines of js

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 //no comment
 const teoria = require("teoria");
 
-var prova = teoria.note('a4');
+var chord = teoria.note('C');
 
-chord = prova.chord('sus2').name;
+class Chord {
+       constructor(chord, tonality) {
+           this.chord = chord;
+           this.tonality = tonality;
+       }
 
+    get chord() {
+        return this._chord;
+    }
+
+    set chord(value) {
+        this._chord = teoria.chord(value);
+    }
+
+
+    get tonality() {
+        return this._tonality;
+    }
+
+    set tonality(value) {
+        this._tonality = teoria.scale(value.split(' ')[0], value.split(' ')[1]);
+    }
+}
+
+var firstChord = new Chord('Cmin9', 'F major');
+
+class bar{
+    constructor(type) {
+        this.array = [];
+        this.type = type;
+    }
+
+    createBar(){
+        for(var  i = 0; i < this.type; i++) {
+            let p = new Chord('C', 'C major');
+            this.array.push(p);
+        }
+    }
+
+    get allChords(){
+        return this.array;
+    }
+}
+
+var newBar = new bar(4);
+
+newBar.createBar()
+
+var chords_in_a_bar = newBar.allChords;
+
+(chords_in_a_bar.forEach(i => console.log(i.chord.notes().toString())))
